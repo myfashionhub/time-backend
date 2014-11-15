ddb9594aab3b3d3947fd41f4c080d74e6339d6be
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-11-15 16:18:49 -0500

    Refactor to add feed name

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,13 +4,24 @@ class Article < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   def self.aggregate
-    feed_urls = Article.feeds
-    GoogleFeed.fetch_articles(feed_urls)
+    feeds = Article.feeds
+    GoogleFeed.fetch_articles(feeds)
   end
 
   def self.feeds
-    feed_urls = ['http://time.com/newsfeed/feed/',
-      'http://rss.cnn.com/rss/money_latest.rss'
+    feeds = [
+      { url: 'http://time.com/newsfeed/feed/',
+        publication: 'Time Magazine'
+      },
+      { url: 'http://rss.cnn.com/rss/money_latest.rss',
+        publication: 'Fortune'
+      },
+      { url: 'http://feeds.people.com/people/headlines',
+        publication: 'People'
+      },
+      { url: 'http://feeds.ew.com/entertainmentweekly/latest',
+        publication: 'Entertainment Weekly'
+      }
     ]
   end
 
--- lib/google_feed.rb
+++ lib/google_feed.rb
@@ -2,24 +2,20 @@ require 'open-uri'
 
 module GoogleFeed
 
-  def self.fetch_articles(feed_urls)
+  def self.fetch_articles(feeds)
     # Fetch articles from an array of rss feed urls
     GoogleAjax.api_key = 'AIzaSyBiCL446e7JMaa8tTTtMcatRnkqBhN2Fzg'
     GoogleAjax.referer = 'http://time-back.herokuapp.com'
     articles = []
+    puts feeds
+    feeds.each do |feed|
+      feed_content = GoogleAjax::Feed.load(URI::encode(feed[:url]), {num: 10})
+      publication = feed[:publication]
 
-    feed_urls.each do |feed_url|
-      feed = GoogleAjax::Feed.load(URI::encode(feed_url), {num: 5})
-      publisher = feed[:description]
-
-      feed[:entries].each do |entry|
-        if entry[:content].length > 500
-          extract = entry[:content_snippet]
-        else
-          extract = entry[:content]
-        end
+      feed_content[:entries].each do |entry|
+        extract = entry[:content] || entry[:content_snippet]
         articles.push({
-          publisher: publisher,
+          publication: publication,
           title: entry[:title],
           url: entry[:link],
           date_published: entry[:published_date],

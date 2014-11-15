e93ab6e5161c37413c9615fa87a5951b46005927
mntj <mntj11@gmail.com>
2014-11-15 17:07:05 -0500

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/articles' => 'articles#index'
+
+  resources :tags, only: :index do
+    resources :articles, only: :index
+  end
+
 end
--- db/migrate/20141115212926_add_join_table_for_articles_tags.rb
+++ db/migrate/20141115212926_add_join_table_for_articles_tags.rb
@@ -1,11 +1,9 @@
 class AddJoinTableForArticlesTags < ActiveRecord::Migration
   def change
-
     create_table :articles_tags do |t|
       t.integer :article_id
       t.integer :tag_id
       t.timestamps
     end
-
   end
 end

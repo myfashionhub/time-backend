d7a3653808c3f5ead65f7f3af19b860c8d4a3560
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-11-15 17:00:36 -0500

    Add end point for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
 
   def index
+    tags = Tag.all
+    render json: tags.to_json
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/articles' => 'articles#index'
+  get '/tags' => 'tags#index'
 end

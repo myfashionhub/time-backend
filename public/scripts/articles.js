e5b3fd37fca726c388a2aa48279cd3bed4b67599
Aaron Appelle <aaron.appelle@gmail.com>
2014-11-16 02:10:14 -0500

    Fetching css from external style sheet then applying it to the backend article html objects

--- public/scripts/articles.js
+++ public/scripts/articles.js
@@ -20,7 +20,7 @@ function displayArticles(articles) {
   for (var i = 0; i < articles.length; i++) {
   var article = articles[i];
 	var stories = $('.stories');
-    var storyDiv = $('<div>').addClass('story').css('background-image','url('+article.img_url+')');
+    var storyDiv = $('<div>').addClass('story');
 	var overlay = $('<div>').addClass('overlay');
 	var title = $('<p>').addClass('title').html(article.title)
 	var tldr = $('<p>').addClass('tldr');
@@ -37,5 +37,38 @@ function displayArticles(articles) {
 	overlay.append(title).append(tldr);
 	storyDiv.append(overlay).append(articleDiv);
 	stories.append(storyDiv);
+	storyDiv.css({
+	  "background-image": 'url('+article.img_url+')',
+	  "background-repeat":"no-repeat",
+	  "background-attachment":"center",
+	  "background-position":"center",
+	  "-webkit-background-size": "cover",
+	  "-moz-background-size": "cover",
+	  "-o-background-size": "cover",
+	  "background-size":"100%",
+	  "width":"100%",
+	  "text-align":"center",
+	  "font-family":"'Open Sans',Helvetica,Arial,sans-serif",
+	  "color":"white",
+	  "vertical-align":"middle",
+	  "padding":"0px",
+	  "margin":"0px"
+    });
+	 title.css({
+	  "font-size":"3em",
+	  "font-family": "Garamond",
+	  "font-weight": "bold",
+	  "position":"relative",
+	  "top":"31%"
+	});
+	tldr.css({
+	  "position":"absolute";
+	  text-align:right;
+	  bottom: 0px;
+	  right:10px;
+	  font-size:2em;
+	  font-family:"Courier New",Courier,serif;
+	  margin-bottom: 0px;
+	});
   }
 }

8ecc1f328c576acfb7079069f658442ec69c6200
Aaron Appelle <aaron.appelle@gmail.com>
2014-11-16 02:48:53 -0500

    renamed a css link in expand.js to have story animations work (i think)

--- public/scripts/expand.js
+++ public/scripts/expand.js
@@ -1,12 +1,12 @@
 var expand = function(){
-	$('.stories div').click(function(){
+	$('.story').click(function(){
 		$(this).children('.article').slideToggle(500);
 	});
 	
 }
 
 var hover = function(){
-	$('.stories div').children('.overlay').hover(
+	$('.story').children('.overlay').hover(
 	function(){
 	   $(this).css({ 'background-color' : 'rgba(0,0,0,0)'});
 	   }, 

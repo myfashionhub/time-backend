b8d2cc29506a52c03f7764b79842f254062eea20
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-11-15 19:56:48 -0500

    Add tag id to data

--- public/scripts/tags.js
+++ public/scripts/tags.js
@@ -12,8 +12,9 @@ function getTags() {
 function displayTags(tagArray) {
   $('.tag_area').empty();
   for (var i = 0; i < tagArray.length; i++) {
-    var tag = $('<li>').addClass('tags');
-    tag.html(tagArray[i].name);
-    tag.appendTo($('.tag_area'));
+    var tag = tagArray[i];
+    var tagLi = $('<li>').addClass('tags');
+    tagLi.html(tag.name).attr('data', tag.id);
+    tagLi.appendTo($('.tag_area'));
   }
 }

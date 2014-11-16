86503aa1765cbf4df2656afb066ca76e8ba85085
Aaron Appelle <aaron.appelle@gmail.com>
2014-11-16 07:29:10 -0500

    brute force

--- public/scripts/articles.js
+++ public/scripts/articles.js
@@ -2,7 +2,8 @@ function getAllArticles(tagArray) {
   $('.story').hide(0);
   var numselect = 0;
   for (var i = 0; i < tagArray.length; i++) {
-	 /*alert(tagArray[i].attr('class'));*/
+	  alert(tagArray[i]);
+	  alert($(tagArray[i]).hasClass('selected'));
 	 if(($(tagArray[i]).hasClass('selected'))){
 		    getArticle(tagArray[i].id); 
 			alert('selected statement');

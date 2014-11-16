0d49f0bac3982a70c6556cdd49f94dff7f9e6031
Aaron Appelle <aaron.appelle@gmail.com>
2014-11-16 05:01:28 -0500

    shivs version

--- public/scripts/sharedcount.js
+++ public/scripts/sharedcount.js
@@ -1,123 +1,49 @@
-function sharedCount(url) {
- url = encodeURIComponent(url);
- alert('url here');
- var domain = "//free.sharedcount.com/"; /* SET DOMAIN */
- var apikey = "0b9f43414d786773d90a6a6fd376247cfe218895" /*API KEY HERE*/
- $.ajax({
-		url : url,
-		apikey : apikey,
-		type: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			if(data.Twitter+data.Facebook.share_count < 50) {
-				alert('minheight here');
-				var minHeight = '225px';
-			}
-			else if(data.Twitter+data.Facebook.share_count < 100) {
-				alert('minheight here');
-				var minHeight = '275px';
-			}
-			else if(data.Twitter+data.Facebook.share_count < 150) {
-				alert('minheight here');
-				var minHeight = '325px';
-			}
-			else if(data.Twitter+data.Facebook.share_count < 200) {
-				alert('minheight here');
-				var minHeight = '475px';
-			}
-			else {
-				alert('minheight here');
-				var minHeight = '500px';
-			}
-			return minHeight; }
- 	});
-}
-	 /*
-   url: domain + "/url",
-   cache: true,
-   dataType: "json",
-   if ('witCredentials' in new XMLHttpRequest) {
- 		success : fn;
- 	},
- 	else {
-	 	alert("what is happening??");
- 	var cb = "sc_" + url.replace(/\W/g, ''),
- window[cb] = fn,
- arg.jsonpCallback = cb,
- arg.dataType += "p"
- }
- /*
-	success: fn
-  });
- /*var arg = {
-	 data: {
-		url : url,
-		apikey : apikey
-	 },
-   url: domain + "/url",
-   cache: true,
-   dataType: "json"
- };*/
- /*
+jQuery.sharedCount = function(url, fn) {
+ url = encodeURIComponent(url || location.href);
+ var domain = "//free.sharedcount.com"; /* SET DOMAIN */
+ var apikey = "abad4f8685f3613e0df148faf025070029ee37cc" /*API KEY HERE*/
+ var arg = { 
+ data: {
+ url: "http://google.com",
+ apikey: apikey
+ },
+ url: domain + "/url",
+ cache: true,
+ dataType: "json"
+ };
  if ('withCredentials' in new XMLHttpRequest) {
  arg.success = fn;
- }
- else {
-	 alert("what is happening??");
+ } else {
  var cb = "sc_" + url.replace(/\W/g, '');
  window[cb] = fn;
  arg.jsonpCallback = cb;
  arg.dataType += "p";
  }
  return jQuery.ajax(arg);
-};*/
-
-/*
-function getAllUrls(tagArray) {
-  for (var i = 0; i < tagArray.length; i++) {
-    getUrl(tagArray[i].id);
-  }
-}
-
-function getUrl(tag_id) {
-	$.ajax({
-		url: '/tags/'+tag_id+'/articles',
-		type: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data)
-			getHeight(data);
-		}
-	});
-}
-*/
-
+};
+$.sharedCount(location.href, function(data){
+ console.log(data.Twitter); 
+       console.log(data.Facebook.like_count);
+	   alert(data.Twitter);
+});
 
 function getHeight(article) {
-	alert('getting height...');
-	var minHeight = sharedCount(article.url);
-	return minHeight;/*, function(data){
-	alert("inside the fn method in getheight");
+	$.sharedCount(article.url, function(data){
 	if(data.Twitter+data.Facebook.share_count < 50) {
-		alert('minheight here');
 		var minHeight = '225px';
 	}
 	else if(data.Twitter+data.Facebook.share_count < 100) {
-		alert('minheight here');
 		var minHeight = '275px';
 	}
 	else if(data.Twitter+data.Facebook.share_count < 150) {
-		alert('minheight here');
 		var minHeight = '325px';
 	}
 	else if(data.Twitter+data.Facebook.share_count < 200) {
-		alert('minheight here');
 		var minHeight = '475px';
 	}
 	else {
-		alert('minheight here');
 		var minHeight = '500px';
 	}
+	});
 	return minHeight;
-	});*/
 }

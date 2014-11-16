f84a29c64e8cb99455df40b6d49264c6b3fd18ec
Aaron Appelle <aaron.appelle@gmail.com>
2014-11-15 20:50:35 -0500

    expanded the expand function

--- public/scripts/expand.js
+++ public/scripts/expand.js
@@ -1,5 +1,10 @@
 var expand = function(){
 	$('.stories div').click(function(){
-		$(this).children('.article').toggle(500)
+		$(this).children('.article').slideToggle(500)
+		if($(this).children('.article').isVisible()){
+			$(this).children('.overlay').css({
+      "background-color": "rgba(0,0,0,0)"});
+		}
 	});
+	
 }

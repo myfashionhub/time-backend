7ac40b6b2bbf7fbb69056d70c849feb0c0aedc5f
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-11-24 23:32:55 -0500

    Show article when clicking on tldr

--- public/scripts/expand.js
+++ public/scripts/expand.js
@@ -1,7 +1,8 @@
 function toggleContent(e) {
 	var targetClass = $(e.target).attr('class'),
 			hidden;
-	if (targetClass.indexOf('title') > -1 || targetClass.indexOf('tldr')) {
+
+	if (targetClass.indexOf('title') > -1 || targetClass.indexOf('tldr') > -1) {
 		hidden = $(e.target).parent().parent().find('.article');
 	} else {
 		hidden = $(e.target).parent().find('.article');

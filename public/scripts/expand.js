7d20d54cfd27ce0a394cade4709693ec5a4e3c44
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-11-24 23:34:20 -0500

    Make toggleContent more concise

--- public/scripts/expand.js
+++ public/scripts/expand.js
@@ -1,13 +1,6 @@
 function toggleContent(e) {
-	var targetClass = $(e.target).attr('class'),
-			hidden;
-
-	if (targetClass.indexOf('title') > -1 || targetClass.indexOf('tldr') > -1) {
-		hidden = $(e.target).parent().parent().find('.article');
-	} else {
-		hidden = $(e.target).parent().find('.article');
-	}
-
+	console.log(this)
+	var hidden = $(this).find('.article');
 	if (hidden.css('display') === 'none') {
 		hidden.slideDown();
 	} else {
